53aa329c5329e5b6312cdb9bc13a0917ccbc1acb
SammedDoshi03 <sammed.doshi.computer.2018@vpkbiet.org>
2022-03-30 14:28:15 +0530

    Added Sorting for Category

--- src/controller/categoryController.ts
+++ src/controller/categoryController.ts
@@ -9,7 +9,8 @@ export default class categoryController{
      * @description - This method is used to get all categories
      */
     public static async getAllCategories() : Promise<ICategory[]>{
-            const categoryList = await categories.find().lean();
+        // sort by name
+            const categoryList = await categories.find().sort({name:1}).lean();
             if(categoryList.length > 0) {
                 return categoryList;
             } else {
